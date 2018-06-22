9e839102995d48507ce5b91a2f0d7d8229686585
Philphanguyen <37415137+Philphanguyen@users.noreply.github.com>
2018-06-21 21:15:29 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
+    $(document).on("click", "#searcher", function() {
+    var inputURL=$("input").val();
+    console.log(inputURL);
 
-  var apiTest = "In the recent case by CNN, they made up their own numbers by using a very broad definition of school shooting,” David Katz, CEO of Global Security and a former special agent with the DEA, told Fox News.In the case of all those school shootings, Katz, who says this sort of study produces a false impression through “deceptive inclusion,” notes CNN’s report includes things such as BB guns. And it’s not necessarily the kind of gun violence that the headlines conjure up. According to Katz, the numbers include any shooting “that just happens to be on school property after hours with no real connection to the type of shootings we are all horrified at."
-
-  var queryURL = "https://proxy.api.deepaffects.com/text/generic/apisearch?q=" +
-        apiTest + "&api_key=bUzQX7myGQFCTinByXbdoKwq9BpYnRd7";
-
-  $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        // After data comes back from the request
-        .then(function(response) {
-          console.log(queryURL);
-
-          console.log(response);
-        });
+    var articleToSummarize = inputURL;
+    var queryUrl = "https://cors-anywhere.herokuapp.com/" + "api.smmry.com/SM_API_KEY=CB55D94259&SM_URL=" + articleToSummarize + "&SM_IGNORE_LENGTH&SM_WITH_BREAK";
+ 
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+ 
+    }).then(function(response) {
+        console.log(response);
+        var sumDiv = $("<div>");
+        var results = response.data;
+        var p = $("<p>").text(response.sm_api_content);
+        sumDiv.append(p);
+        $("#AIOutput").append(sumDiv);
+    });
+    });
 
 });
