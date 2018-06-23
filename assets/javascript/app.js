b42fbadd82167521f7db11ab271d45b7ca4f3ede
Twilight <edwardsjacob1830@gmail.com>
2018-06-23 18:30:38 -0400

    6/23 at 6:30 p.m.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,57 @@
 $(document).ready(function() {
     $(".resultsDisplay").hide();
 
+    var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".modal-close");
+
+    function toggleModal() {
+      modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+      if (event.target === modal) {
+        toggleModal();
+      }
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+    
+
+
+
     //********************SMMRY API on click******************* */
     $(document).on("click", "#searcher", function() {
       var inputURL=$("input").val();
       $(".hero").hide(1000);
       $(".resultsDisplay").show(1000);
       var articleToSummarize = inputURL;
-      var queryUrl = "https://cors-anywhere.herokuapp.com/" + "api.smmry.com/SM_API_KEY=CB55D94259&SM_URL=" + articleToSummarize + "&SM_IGNORE_LENGTH";
- 
-      $.ajax({
-        url: queryUrl,
-        method: "GET",
- 
-      }).then(function(response) {
-        console.log(response);
-        var sumDiv = $("<div>");
-        var results = response.data;
-        var p = $("<p>").text(response.sm_api_content);
-        sumDiv.append(p);
-        $("#summary-output").append(sumDiv);
+
+      $("#URL").on("click", function() {
+        window.open(articleToSummarize,  "_blank");
+      })
+      
+
+      $.post(
+        'https://apiv2.indico.io/summarization',
+        JSON.stringify({
+          'api_key': "4f4722e7847cae008684275f830abf12",
+          'data': articleToSummarize,
+          'top_n': 10,
+        })
+      ).then(function(res1) { 
+        console.log(res1) ;
+        var summaryObject = JSON.parse(res1);
+        console.log(summaryObject);
+        $("#summary-output").text(summaryObject.results);     
       });
+
     //***************Indico API************************ */
-    // batch example
+
+    // Obtains sentiment output for article input
+
       $.post(
         'https://apiv2.indico.io/emotion/',
         JSON.stringify({
@@ -35,16 +64,34 @@ $(document).ready(function() {
         var emotionObject = JSON.parse(res);
         console.log(emotionObject);
         
-        $("#first-output").text(emotionObject.results.anger);
-        $("#second-output").text(emotionObject.results.joy);
-        $("#third-output").text(emotionObject.results.fear);
-        $("#fourth-output").text(emotionObject.results.sadness);
-        $("#fifth-output").text(emotionObject.results.surprise);
+        $("#first-output").text(emotionObject.results.anger.toFixed(2));
+        $("#second-output").text(emotionObject.results.joy.toFixed(2));
+        $("#third-output").text(emotionObject.results.fear.toFixed(2));
+        $("#fourth-output").text(emotionObject.results.sadness.toFixed(2));
+        $("#fifth-output").text(emotionObject.results.surprise.toFixed(2));
+      });
+
+    //obtains political output for article input
+
+      $.post(
+        'https://apiv2.indico.io/political',
+        JSON.stringify({
+          'api_key': "4f4722e7847cae008684275f830abf12",
+          'data': articleToSummarize,
+          'threshold': 0.25
+        })
+      ).then(function(res2) { 
+        console.log(res2);
+        var politicalObject = JSON.parse(res2);
+        console.log(politicalObject);
+        $("#liberal-output").text(politicalObject.results.Liberal.toFixed(2));
+        $("#conservative-output").text(politicalObject.results.Conservative.toFixed(2));
+        $("#libertarian-output").text(politicalObject.results.Libertarian.toFixed(2));
+        $("#green-output").text(politicalObject.results.Green.toFixed(2));
+      
       });
     });
-
+  });
 
 
     //************************** Display *************************
-
-});
