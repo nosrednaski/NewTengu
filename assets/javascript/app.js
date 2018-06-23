d87cce2f243ee2b77175450da524b775fa693e40
nosrednaski <nosrednaski@gmail.com>
2018-06-23 12:57:42 -0400

    Hiding and showing JS. HTML results tiles.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,31 @@
 $(document).ready(function() {
+    $(".resultsDisplay").hide();
+
     //********************SMMRY API on click******************* */
     $(document).on("click", "#searcher", function() {
-    var inputURL=$("input").val();
-    console.log(inputURL);
-
-    var articleToSummarize = inputURL;
-    var queryUrl = "https://cors-anywhere.herokuapp.com/" + "api.smmry.com/SM_API_KEY=CB55D94259&SM_URL=" + articleToSummarize + "&SM_IGNORE_LENGTH&SM_WITH_BREAK";
+      var inputURL=$("input").val();
+      console.log(inputURL);
+      $(".hero").hide(1000);
+      $(".resultsDisplay").show(1000);
+      var articleToSummarize = inputURL;
+      var queryUrl = "https://cors-anywhere.herokuapp.com/" + "api.smmry.com/SM_API_KEY=CB55D94259&SM_URL=" + articleToSummarize + "&SM_IGNORE_LENGTH&SM_WITH_BREAK";
  
-    $.ajax({
+      $.ajax({
         url: queryUrl,
         method: "GET",
  
-    }).then(function(response) {
+      }).then(function(response) {
         console.log(response);
         var sumDiv = $("<div>");
         var results = response.data;
         var p = $("<p>").text(response.sm_api_content);
         sumDiv.append(p);
         $("#AIOutput").append(sumDiv);
-    });
+      });
     });
 
-    //***************theySay API************************ */
-    
-    $.post(
-        'https://apiv2.indico.io/emotion',
-        JSON.stringify({
-          'api_key': "52ab40ad3535532ada2f2b567ddb900e",
-          'data': "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)",
-          'threshold': 0.1
-        })
-      ).then(function(res) { console.log(res) });
-      
-      // batch example
+    //***************Indico API************************ */
+    // batch example
       $.post(
         'https://apiv2.indico.io/emotion/batch',
         JSON.stringify({
@@ -41,4 +34,22 @@ $(document).ready(function() {
           'threshold': 0.1
         })
       ).then(function(res) { console.log(res) });
+
+    // batch example
+      $.post(
+       'https://apiv2.indico.io/political/batch',
+       JSON.stringify({
+         'api_key': "52ab40ad3535532ada2f2b567ddb900e",
+         'data': [
+           "I have a constitutional right to bear arms!",
+           "I wish more candidates cared about the environment."
+         ],
+         'threshold': 0.25
+        })
+       ).then(function(res) { console.log(res) });
+
+
+
+    //************************** Display *************************
+
 });
