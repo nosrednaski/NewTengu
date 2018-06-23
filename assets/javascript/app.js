d889277ad0296d75ff518aa73a82cab06fb7195c
Twilight <edwardsjacob1830@gmail.com>
2018-06-23 13:44:32 -0400

    let's hope this works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,10 @@ $(document).ready(function() {
     //********************SMMRY API on click******************* */
     $(document).on("click", "#searcher", function() {
       var inputURL=$("input").val();
-      console.log(inputURL);
       $(".hero").hide(1000);
       $(".resultsDisplay").show(1000);
       var articleToSummarize = inputURL;
-      var queryUrl = "https://cors-anywhere.herokuapp.com/" + "api.smmry.com/SM_API_KEY=CB55D94259&SM_URL=" + articleToSummarize + "&SM_IGNORE_LENGTH&SM_WITH_BREAK";
+      var queryUrl = "https://cors-anywhere.herokuapp.com/" + "api.smmry.com/SM_API_KEY=CB55D94259&SM_URL=" + articleToSummarize + "&SM_IGNORE_LENGTH";
  
       $.ajax({
         url: queryUrl,
@@ -20,33 +19,29 @@ $(document).ready(function() {
         var results = response.data;
         var p = $("<p>").text(response.sm_api_content);
         sumDiv.append(p);
-        $("#AIOutput").append(sumDiv);
+        $("#summary-output").append(sumDiv);
       });
-    });
-
     //***************Indico API************************ */
     // batch example
       $.post(
-        'https://apiv2.indico.io/emotion/batch',
+        'https://apiv2.indico.io/emotion/',
         JSON.stringify({
           'api_key': "52ab40ad3535532ada2f2b567ddb900e",
-          'data': ["I did it. I got into Grad School. Not just any program, but a GREAT program. :-)", "Like seriously my life is bleak, I have been unemployed for almost a year."],
+          'data': articleToSummarize,
           'threshold': 0.1
         })
-      ).then(function(res) { console.log(res) });
-
-    // batch example
-      $.post(
-       'https://apiv2.indico.io/political/batch',
-       JSON.stringify({
-         'api_key': "52ab40ad3535532ada2f2b567ddb900e",
-         'data': [
-           "I have a constitutional right to bear arms!",
-           "I wish more candidates cared about the environment."
-         ],
-         'threshold': 0.25
-        })
-       ).then(function(res) { console.log(res) });
+      ).then(function(res) { 
+        console.log(res);
+        var emotionObject = JSON.parse(res);
+        console.log(emotionObject);
+        
+        $("#first-output").text(emotionObject.results.anger);
+        $("#second-output").text(emotionObject.results.joy);
+        $("#third-output").text(emotionObject.results.fear);
+        $("#fourth-output").text(emotionObject.results.sadness);
+        $("#fifth-output").text(emotionObject.results.surprise);
+      });
+    });
 
 
 
